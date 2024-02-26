163c5954285e776007336d03a109cccbdca26ce1
Zachary Michael Hugo A. Reyes <111546468+Porkchop15@users.noreply.github.com>
2024-02-26 21:47:51 +0800

    Create Instructions3.java

---
+++ java/Instructions3.java
@@ -0,0 +1,28 @@
+package com.example.dropballproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Instructions3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions3);
+    }
+
+    public void goToInstructions2(View v){
+        Intent i = new Intent(this, Instructions2.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void goToInstructions4(View v){
+        Intent i = new Intent(this, Instructions4.class);
+        startActivity(i);
+        finish();
+    }
+}
